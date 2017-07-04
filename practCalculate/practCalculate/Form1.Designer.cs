7ea72b3dd6205a605919d5c398dce6a346b89fbf
scout55 <kemiiep.omck@mail.ru>
2017-07-04 14:44:33 +0600

    simple function added.

--- practCalculate/practCalculate/Form1.Designer.cs
+++ practCalculate/practCalculate/Form1.Designer.cs
@@ -47,6 +47,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "+";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
@@ -88,6 +89,7 @@
             this.button2.TabIndex = 8;
             this.button2.Text = "-";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -97,6 +99,7 @@
             this.button3.TabIndex = 9;
             this.button3.Text = "*";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -106,6 +109,7 @@
             this.button4.TabIndex = 10;
             this.button4.Text = "/";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // label3
             // 
--- practCalculate/practCalculate/Form1.cs
+++ practCalculate/practCalculate/Form1.cs
@@ -12,11 +12,43 @@ namespace practCalculate
 {
     public partial class Form1 : Form
     {
+        double numberFirst;
+        double numberSecond;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            numberFirst = Convert.ToDouble(textBox1.Text);
+            numberSecond = Convert.ToDouble(textBox2.Text);
+            label1.Text = Convert.ToString(numberFirst + numberSecond);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            numberFirst = Convert.ToDouble(textBox1.Text);
+            numberSecond = Convert.ToDouble(textBox2.Text);
+            label1.Text = Convert.ToString(numberFirst - numberSecond);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            numberFirst = Convert.ToDouble(textBox1.Text);
+            numberSecond = Convert.ToDouble(textBox2.Text);
+            label1.Text = Convert.ToString(numberFirst * numberSecond);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            numberFirst = Convert.ToDouble(textBox1.Text);
+            numberSecond = Convert.ToDouble(textBox2.Text);
+            if (numberSecond == 0) label1.Text = "error";
+            else label1.Text = Convert.ToString(numberFirst / numberSecond);
+        }
     }
 }
