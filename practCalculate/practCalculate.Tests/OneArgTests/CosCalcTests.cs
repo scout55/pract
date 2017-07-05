ed2df87d3a661894501f8e5c66bc0b9f0d3c865e
scout55 <kemiiep.omck@mail.ru>
2017-07-05 15:10:19 +0600

    First test is added.

---
+++ practCalculate/practCalculate.Tests/OneArgTests/CosCalcTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace practCalculate.Tests.OneArgTests
+{
+    [TestFixture]
+    public class CosCalcTests
+    {
+        [TestCase(0, 1)]
+        [TestCase(90, -0.4480)]
+        [TestCase(45, 0.5253)]
+        public void CalculateTest(double arg,double expected)
+        {
+            var cosCalculator = new CosCalc();
+            var result = cosCalculator.Calculate(arg);
+            Assert.AreEqual(result, expected,0.0001);
+        }
+    }
+
+}
+
